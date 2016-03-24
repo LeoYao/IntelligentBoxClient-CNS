44bbc483fd9fdae174d220e4bae3cd49aaf2b478
Leo Yao <leo.yao.dev@gmail.com>
2016-03-24 17:44:58 -0400

    push Bootstrapper

---
+++ src/main/java/intelligentBoxClient/cns/bootstrapper/BootStrapper.java
@@ -0,0 +1,25 @@
+package intelligentBoxClient.cns.bootstrapper;
+
+import intelligentBoxClient.cns.dao.IDynamoDbContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yaohx on 3/24/2016.
+ */
+@Component
+public class Bootstrapper {
+
+    private IDynamoDbContext _ctx;
+
+    @Autowired
+    public Bootstrapper(IDynamoDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public void startup()
+    {
+        _ctx.initialize();
+    }
+}
